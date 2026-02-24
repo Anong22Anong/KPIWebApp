2b50e0edba9bcb57e0ae2fe25587164196a25919
Anong <anongnard.lapsungnoen22@gmail.com>
2026-02-24 18:35:32 +0700

    fix routing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -46,7 +46,7 @@ public class HomeController : Controller
         return View(viewModel);
     }
 
-    public IActionResult Privacy()
+    public IActionResult Index()
     {
         return View();
     }
